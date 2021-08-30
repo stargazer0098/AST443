c837a1078364bbe4bc357c0ddc46e00358f33056
amlee21 <89752312+amlee21@users.noreply.github.com>
2021-08-30 18:19:58 -0400

    Update empty.py

--- empty.py
+++ empty.py
@@ -1,10 +1,11 @@
 import numpy as np 
-import SHAH
 
 # We are in the northern hemisphere, so if the (declination - latitude) > 90 degrees, it will never be visible (star will not rise enough for us to see it)
 # >> consider only candidates where (declination + Stony's latitude) > 90  
 
 # RA from 22hr to 1hr would have best viewing at around 10pm to 1am 
+
+data = np.loadtxt()
 
 
 
